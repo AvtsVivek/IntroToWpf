cf2f473d020a5a1714b2e8b5419051f9f072891e
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-06-19 10:39:38 +0530

    Added more info

--- src/apps/950510-ListBoxScrollViewer/ViewModels/UserControl2ViewModel.cs
+++ src/apps/950510-ListBoxScrollViewer/ViewModels/UserControl2ViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 using ListBoxScrollViewer.Infra;
 
@@ -7,12 +8,22 @@ namespace ListBoxScrollViewer.ViewModels
     public class UserControl2ViewModel : BaseViewModel, IPageViewModel
     {
         public event EventHandler<EventArgs<string>>? ViewChanged;
+
+        public ObservableCollection<string> MyStrings { get; }
+
         public string PageId { get; set; }
         public string Title { get; set; } = "View 2";
 
         public UserControl2ViewModel(string pageIndex = "2")
         {
             PageId = pageIndex;
+
+            MyStrings = new ObservableCollection<string>();
+
+            for (int i = 0; i < 15; i++)
+            {
+                MyStrings.Add("This is a long sample text to test a listbox scroller...");
+            }
         }
     }
 }
