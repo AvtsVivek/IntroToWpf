fe154cbf95325b65448a068da965300910be8e58
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-06-19 10:53:13 +0530

    Added more info

--- src/apps/950510-ListBoxScrollViewer/ViewModels/UserControl3ViewModel.cs
+++ src/apps/950510-ListBoxScrollViewer/ViewModels/UserControl3ViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 using ListBoxScrollViewer.Infra;
 
@@ -7,12 +8,22 @@ namespace ListBoxScrollViewer.ViewModels
     public class UserControl3ViewModel : BaseViewModel, IPageViewModel
     {
         public event EventHandler<EventArgs<string>>? ViewChanged;
+        public ObservableCollection<string> MyStrings { get; }
         public string PageId { get; set; }
         public string Title { get; set; } = "View 3";
 
         public UserControl3ViewModel(string pageIndex = "3")
         {
             PageId = pageIndex;
+
+            MyStrings = new ObservableCollection<string>();
+
+            var stringSample = "This is a very very very very very very very very very very very long long long long long long long long long long long long long long long long long long sample text to test a listbox scroller...";
+            stringSample = stringSample + stringSample + stringSample;
+            for (int i = 0; i < 15; i++)
+            {
+                MyStrings.Add(stringSample);
+            }
         }
     }
 }
