9e5b8aee528d53de926980ae24f332ae01b64465
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-09-07 15:07:35 +0530

    Renamed the folders

--- src/tasks/300500-BasicMvvm/commands.sh
+++
@@ -1,8 +0,0 @@
-cd ../../..
-
-cd src/tasks/300500-BasicMvvm
-
-cd src/apps/300500-BasicMvvm
-
-dotnet run --project ./BasicMvvm.csproj
-
--- src/tasks/300510-BasicMvvmVarOne/commands.sh
+++
@@ -1,8 +0,0 @@
-cd ../../..
-
-cd src/tasks/300510-BasicMvvmVarOne
-
-cd src/apps/300510-BasicMvvmVarOne
-
-dotnet run --project ./BasicMvvmVarOne.csproj
-
