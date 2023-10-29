42beec909f787c39540b32e524973485b44b1e1a
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-10-29 14:31:38 +0530

    Renamed the files

--- src/tasks/175100-IgnorableAttributeIntro/commands.sh
+++
@@ -1,8 +0,0 @@
-cd ../../..
-
-cd src/tasks/175100-IgnorableAttributeIntro
-
-cd src/apps/175100-IgnorableAttributeIntro
-
-dotnet run --project ./175100-IgnorableAttributeIntro.csproj
-
--- src/tasks/200200-BasicWpfApp/commands.sh
+++
@@ -1,8 +0,0 @@
-cd ../../..
-
-cd src/tasks/200200-BasicWpfApp
-
-cd src/apps/200200-BasicWpfApp
-
-dotnet run --project ./BasicWpfApp.csproj
-
